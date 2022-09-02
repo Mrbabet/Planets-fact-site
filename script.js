66471a55efdfa160b095dd67e5ec0261fd24c07a
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2022-09-02 15:06:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector(".burger-bars");
 const hiddenMenu = document.querySelector(".nav-mobile");
 const bodyShade = document.querySelector("body");
+const overviewBtn = document.querySelector(".overview-btn");
+const structureBtn = document.querySelector(".structure-btn");
+const geologyBtn = document.querySelector(".geology-btn");
+const overview = document.querySelector(".overview");
+const structure = document.querySelector(".internal-structure");
+const geology = document.querySelector(".surface-geology");
 
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
@@ -16,4 +22,14 @@ menuBtn.addEventListener("click", () => {
 menuBtn.addEventListener("click", function () {
   hiddenMenu.classList.toggle("active");
   bodyShade.classList.toggle("active");
+});
+
+overviewBtn.addEventListener("click", function () {
+  overview.classList.toggle("active");
+});
+structureBtn.addEventListener("click", function () {
+  structure.classList.toggle("active");
+});
+geologyBtn.addEventListener("click", function () {
+  geology.classList.toggle("active");
 });
