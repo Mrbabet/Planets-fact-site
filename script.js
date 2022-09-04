fa9a581a3cef6258786233583ed20dc2f2bf9bd8
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2022-09-04 19:43:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const geologyBtn = document.querySelector(".geology-btn");
 const overview = document.querySelector(".overview");
 const structure = document.querySelector(".internal-structure");
 const geology = document.querySelector(".surface-geology");
+const overviewBtnDesktop = document.querySelector(".overview-btn-desktop");
+const structureBtnDesktop = document.querySelector(".structure-btn-desktop");
+const geologyBtnDesktop = document.querySelector(".geology-btn-desktop");
 
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
@@ -24,12 +27,31 @@ menuBtn.addEventListener("click", function () {
   bodyShade.classList.toggle("active");
 });
 
+// let js = document.querySelectorAll(".mobile-overview-section.overview-btn");
+// js.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     let activeClass = document.querySelector(".active");
+//     activeClass.className = activeClass.className.replace("active", "");
+//     item.className += "active";
+//   });
+// });
+
 overviewBtn.addEventListener("click", function () {
-  overview.classList.toggle("active");
+  overview.classList.remove("overview-inactive");
+  structure.classList.remove("structure-active");
+  structure.classList.remove("structure-inactive");
+  geology.classList.remove("geology-active");
+  geology.classList.remove("geology-inactive");
 });
 structureBtn.addEventListener("click", function () {
-  structure.classList.toggle("active");
+  structure.classList.toggle("structure-active");
+  overview.classList.toggle("overview-inactive");
+  geology.classList.toggle("geology-inactive");
+  geology.classList.remove("geology-active");
 });
 geologyBtn.addEventListener("click", function () {
-  geology.classList.toggle("active");
+  geology.classList.toggle("geology-active");
+  geology.classList.remove("geology-inactive");
+  structure.classList.remove("structure-active");
+  overview.classList.remove("overview-active");
 });
